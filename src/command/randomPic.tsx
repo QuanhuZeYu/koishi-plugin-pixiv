88f7ea0488a493e8fd1bd907b40a3ed626fc7802
泉户泽雨 <1624910218@qq.com>
2024-09-08 17:38:57 +0800

    尝试tsx

---
+++ src/command/randomPic.tsx
@@ -0,0 +1,25 @@
+import { Context, Schema, Session } from "koishi";
+import {} from "@satorijs/element"
+
+import { Argv, h } from "koishi";
+import Data from "../Data/_index";
+import chrome from "../chrome/_index";
+import fs from 'fs'
+
+async function randomTJPic(av:Argv,ms:string) {
+    const logger = Data.baseData.getLogger()
+    const ss = av.session
+    logger.info("正在随机获取图片")
+    try {
+        const picBuffers = await chrome.browser.getRandomTJPic()
+        const message = []
+        for(const [index, pic] of picBuffers.entries()) {
+            ss.send(h.image(pic, 'image/png'))
+        }
+    } catch (e) {
+        logger.warn(`获取失败，失败信息: ${e}`)
+        ss.send("获取失败，请联系管理员查看控制台")
+    }
+}
+
+export default randomTJPic
