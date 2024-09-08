5d43704ccc5dcf93d4882dfc3d73019c9e69e856
泉户泽雨 <1624910218@qq.com>
2024-09-08 20:50:46 +0800

    修复了undefined的错误

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,17 @@ export const inject = {
 
 export interface Config { 
 	// excutePath: string,
-	ensureLogin: boolean
+	ensureLogin: boolean,
+	等待NAV超时时间: number,
 	HTMLSelector: object
 }
 
 export const Config: Schema<Config> = Schema.object({
 	// excutePath: Schema.string().description("puppeteer的chrome路径或者chromium路径").required(),
 	ensureLogin: Schema.boolean().description("请在确认登录过pixiv后再打开此开关!").default(false),
+	等待NAV超时时间: Schema.number().description("等待NAV超时时间，单位ms").default(15000),
 	HTMLSelector: Schema.object({
-		推荐作品URLs选择器: Schema.string().description("如果你不知道这是什么请不要动它！！！推荐页面的图片选择器，编辑一段可以在浏览器运行的代码段，最后返回目标图片urls数组").default("const sections = Array.from(document.querySelectorAll('section'));const tuijianSections = sections.filter((section) =>section.textContent.includes('推荐作品'));const listItems = Array.from(tuijianSections[0].querySelectorAll('li'));const imgEs = [];for(const li of listItems) {const img = li.querySelector('img');if (src) {if (src.includes('i.pximg.net')) {imgEs.push(img.src);}}} imgEs;"),
+		推荐作品URLs选择器: Schema.string().description("如果你不知道这是什么请不要动它！！！推荐页面的图片选择器，编辑一段可以在浏览器运行的代码段，最后返回目标图片urls数组").default("const sections = Array.from(document.querySelectorAll('section'));const tuijianSections = sections.filter((section) =>section.textContent.includes('推荐作品'));const listItems = Array.from(tuijianSections[0].querySelectorAll('li'));const imgEs = [];for(const li of listItems) {const img = li.querySelector('img');if (img && img?.src) {if (img.src.includes('i.pximg.net')) {imgEs.push(img.src);}}} imgEs;"),
 		主图像URLs选择器: Schema.string().description("如果你不知道这是什么请不要动它！！！这个选择器是选择点击进入作品后寻找大图的，最后返回原图像的URLs数组").default("const urls = []; Array.from(document.querySelector('main section figure').querySelectorAll('img')).forEach(img => urls.push(img.src)); urls;")
 	})
 });
