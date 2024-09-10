2519670bc426e3c50551fcf4cb0e9113839c16fe
泉户泽雨 <1624910218@qq.com>
2024-09-10 15:39:53 +0800

    版本更新 -> 0.2.8 | 修复部分bug，尝试支持新功能

--- src/command/randomPic.tsx
+++ src/command/randomPic.tsx
@@ -22,9 +22,9 @@ async function randomTJPic(av:Argv,ms:string) {
         logger.info(`图片获取成功: 数量${picBuffers?.length}`)
         for(const  pic of picBuffers) {
             // message.push(h.image(pic, 'image/png'))
-            <><img src={'data:image/png;base64,' + pic.toString('base64')}/></>
+            message.push(<img src={'data:image/png;base64,' + pic.toString('base64')}/>)
         }
-        message.push(<>{artInfo}</>)
+        message.push(artInfo)
         message.join('')
         message = 
         <message>
