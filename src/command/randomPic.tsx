56f310bab20600b3a70605ec9ca4eb1cb4d416e4
泉户泽雨 <1624910218@qq.com>
2024-09-10 15:07:17 +0800

    添加quote逻辑，引用用户指令原文

--- src/command/randomPic.tsx
+++ src/command/randomPic.tsx
@@ -7,18 +7,30 @@ import chrome from "../chrome/_index";
 import fs from 'fs'
 
 async function randomTJPic(av:Argv,ms:string) {
+    let message
     const logger = Data.baseData.getLogger()
     const ss = av.session
     logger.info("正在随机获取图片")
+    message = <message>
+        <quote id={ss.messageId}/>
+        {'你在此地不要走动'}
+    </message>;
+    await ss.send(message)
+    message = []
     try {
         const [picBuffers, artInfo] = await chrome.browser.getRandomTJPic()
         logger.info(`图片获取成功: 数量${picBuffers?.length}`)
-        const message = []
         for(const  pic of picBuffers) {
-            message.push(h.image(pic, 'image/png'))
+            // message.push(h.image(pic, 'image/png'))
+            <img src={'data:image/png;base64,' + pic.toString('base64')}/>
         }
-        message.push(artInfo)
+        message.push(<>{artInfo}</>)
         message.join('')
+        message = 
+        <message>
+            <quote id={ss.messageId}/>
+            {message}
+        </message>
         await ss.send(message)
     } catch (e) {
         logger.warn(`获取失败，失败信息: ${e}`)
